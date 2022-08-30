c39a39cff7da1a0a1f38701f92f14edcce336707
pedatela <lucaspedatela@gmail.com>
2022-08-30 18:49:50 -0300

    fix: adicionando logica

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
 require('dotenv')
 const app = require('./app');
 
-app.listen(process.env.PORT, () => { console.log(`Up and running on a port: ${process.env.PORT} and host is ${process.env.HOST}`); })
+app.listen(process.env.PORT, () => { console.log(`Up and running on a port: ${process.env.PORT || 5000} and host is ${process.env.HOST || 'https://localhost'}`); })
